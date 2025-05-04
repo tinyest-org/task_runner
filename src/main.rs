8447165e99d4a90b379b70aa236300ceed6f596a
Plawn <plawn@konf.sh>
2025-05-04 11:11:59 +0200

    first commit

---
+++ src/main.rs
@@ -0,0 +1,55 @@
+use actix_web::{App, Error, HttpResponse, HttpServer, web};
+use confik::{Configuration as _, EnvSource};
+use deadpool_postgres::{Client, Pool};
+use dotenvy::dotenv;
+use task_runner::{config::ExampleConfig, db, error::MyError, models::User};
+use tokio_postgres::NoTls;
+
+
+pub async fn get_users(db_pool: web::Data<Pool>) -> Result<HttpResponse, Error> {
+    let client: Client = db_pool.get().await.map_err(MyError::PoolError)?;
+
+    let users = db::get_users(&client).await?;
+
+    Ok(HttpResponse::Ok().json(users))
+}
+
+pub async fn add_user(
+    user: web::Json<User>,
+    db_pool: web::Data<Pool>,
+) -> Result<HttpResponse, Error> {
+    let user_info: User = user.into_inner();
+
+    let client: Client = db_pool.get().await.map_err(MyError::PoolError)?;
+
+    let new_user = db::add_user(&client, user_info).await?;
+
+    Ok(HttpResponse::Ok().json(new_user))
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    dotenv().ok();
+
+    let config = ExampleConfig::builder()
+        .override_with(EnvSource::new())
+        .try_build()
+        .unwrap();
+
+    let pool = config.pg.create_pool(None, NoTls).unwrap();
+    let schema = include_str!("../sql/schema.sql");
+    pool.get().await.unwrap().batch_execute(schema).await.unwrap();
+
+    let server = HttpServer::new(move || {
+        App::new().app_data(web::Data::new(pool.clone())).service(
+            web::resource("/users")
+                .route(web::post().to(add_user))
+                .route(web::get().to(get_users)),
+        )
+    })
+    .bind(config.server_addr.clone())?
+    .run();
+    println!("Server running at http://{}/", config.server_addr);
+
+    server.await
+}
